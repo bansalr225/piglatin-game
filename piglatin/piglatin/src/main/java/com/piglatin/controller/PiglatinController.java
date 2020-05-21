a4aaed3fadcdca02ae2a3ef6745bdc816e331292
Rahul Bansal <rahul.bansal@ATMECS.CORP>
2020-05-21 12:35:30 +0530

    Created all Utility methods

--- piglatin/piglatin/src/main/java/com/piglatin/controller/PiglatinController.java
+++ piglatin/piglatin/src/main/java/com/piglatin/controller/PiglatinController.java
@@ -1,6 +1,8 @@
 package com.piglatin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +17,8 @@ public class PiglatinController {
 	private PigLatinService pigLatinService;
 
 	@RequestMapping(value = "/piglatin", method = RequestMethod.GET)
-	public String translateInLatin(@RequestParam String sentence) {
-		return pigLatinService.translateInLatin(sentence);
+	public ResponseEntity<String> translateInLatin(@RequestParam String sentence) {
+		return new ResponseEntity<String>(pigLatinService.translateInLatin(sentence), HttpStatus.OK);
 	}
 
 }
--- piglatin/piglatin/src/main/java/com/piglatin/service/PigLatinService.java
+++ piglatin/piglatin/src/main/java/com/piglatin/service/PigLatinService.java
@@ -8,12 +8,145 @@ public class PigLatinService {
 
 	public String translateInLatin(String sentence) {
 		validate(sentence);
+		// Split the sentence/paragraph from space.
+		String[] words = sentence.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];
+			if (StringUtils.isEmpty(word) || "".equals(word.trim()))
+				continue;
+			String modifiedString = "";
+			if (isWordContainsHyphen(word)) {
+				String[] hyphenSpilletedWords = word.split("-");
+				for (int j = 0; j < hyphenSpilletedWords.length; j++) {
+					if (j == hyphenSpilletedWords.length - 1)
+						modifiedString += modiFyTheWord(word);
+					else
+						modifiedString += modiFyTheWord(word) + "-";
+				}
+			} else
+				modifiedString = modiFyTheWord(word);
+			words[i] = modifiedString;
+			return words.toString();
+			// return getFinalStringFromWords(words);
+		}
 		return sentence;
 	}
 
-	private void validate(String sentence) {
-		if (sentence == null || StringUtils.isEmpty(sentence))
-			throw new NullPointerException("Sentence can't be null !!");
+	private String getFinalStringFromWords(String[] words) {
+		String finalString = "";
+		for (int i = 0; i < words.length; i++) {
+			if (i == words.length - 1)
+				finalString += words[i];
+			else
+				finalString += words[i] + " ";
+		}
+		return null;
+	}
+
+	/**
+	 * This method is used to modify the word after check all given conditions.
+	 * 
+	 * @param word
+	 * @return it's return final modified pig latin.
+	 */
+	private String modiFyTheWord(String word) {
+		switch (checkConditions(word)) {
+		case 1: {
+			if (containsPuctuation(word))
+				return puctuationContainedModifiedWord(word, "ay", 1);
+			else
+				return addSpecialWord(word, "ay", 1);
+		}
+		case 2: {
+			if (containsPuctuation(word))
+				return puctuationContainedModifiedWord(word, "way", 2);
+			else
+				return addSpecialWord(word, "way", 2);
+		}
+		case 3: {
+			return word;
+		}
+
+		default:
+			return word;
+		}
+	}
+
+	/**
+	 * This method is using for to add special word based on condition like after
+	 * checking that word start with vowel or consonant.
+	 * 
+	 * @param word
+	 * @param specialWord
+	 * @param rule
+	 * @return returning with adding special word based on condition
+	 */
+	private String addSpecialWord(String word, String specialWord, int rule) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * This method is using for punctuation contained word to add special word based
+	 * on condition like after checking that word start with vowel or consonant.
+	 * 
+	 * @param word
+	 * @param specialWord
+	 * @param rule
+	 * @return returning with adding special word based on condition
+	 */
+	private String puctuationContainedModifiedWord(String word, String specialWord, int rule) {
+		return null;
+	}
+
+	/**
+	 * Will check that word may or may not contains punctuation char.
+	 * 
+	 * @param word
+	 * @return
+	 */
+	private boolean containsPuctuation(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Checking word start with vowel or consonant.
+	 * 
+	 * @param word
+	 * @return
+	 */
+	private int checkConditions(String word) {
+		char c = word.charAt(0);
+		if (word.length() >= 3 && "way".equalsIgnoreCase(word.substring(word.length() - 3)))
+			return 3;
+		else if ('a' == c || 'A' == c || 'e' == c || 'E' == c || 'i' == c || 'I' == c || 'o' == c || 'O' == c
+				|| 'u' == c || 'U' == c)
+			return 2;
+		else
+			return 1;
+	}
+
+	/**
+	 * Checking that word contains hyphen or not.
+	 * 
+	 * @param word
+	 * @return
+	 */
+	private boolean isWordContainsHyphen(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * Validating the string
+	 * 
+	 * @param sentence
+	 * @throws NullPointerException
+	 */
+	private void validate(String sentence) throws NullPointerException {
+		if (StringUtils.isEmpty(sentence) || "".equals(sentence.trim()))
+			throw new NullPointerException("Invalid string !!");
 	}
 
 }
